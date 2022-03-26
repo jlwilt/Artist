c96badc10789ae04fd039cc818d5c1f471d6e845
Johnathon Wilt <91701918+pythonithon@users.noreply.github.com>
2022-03-26 18:08:00 -0400

    Update Musician.java

--- Musician.java
+++ Musician.java
@@ -6,7 +6,7 @@ public class Musician extends Artist {
 	
 	public Musician(String[] sc) {
 		name = removeNameUnderscores(sc[0]);
-		name = name.trim();
+		//name = name.trim();
 		country = sc[1];
 		age = Integer.valueOf(removeAgeAsterisk(sc[2]));
 		if (sc[3].equals("T")) {
@@ -19,10 +19,10 @@ public class Musician extends Artist {
 		}
 		instruments = sc[4].split("/");
 		instrument1 = instruments[0];
-		instrument1 = instrument1.trim();
-		instrument2 = instrument2.trim();
+		//instrument1 = instrument1.trim();
+		//instrument2 = instrument2.trim();
 		netWorth = Integer.valueOf(sc[5]);
-		System.out.println(name + " " + country + " " + age + " " + active + " " + instrument1 + " " + instrument2 + " " + netWorth);
+		//System.out.println(name + " " + country + " " + age + " " + active + " " + instrument1 + " " + instrument2 + " " + netWorth);
 	}
 
 }
